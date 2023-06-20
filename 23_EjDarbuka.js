f0d07e1048c8e7af13e5f6feb5e2b55fcda4b465
Mordisquitos <mariojm2612@gmail.com>
2023-06-20 19:29:42 +0200

    DarbukaAnimations

--- 23_EjDarbuka.js
+++ 23_EjDarbuka.js
@@ -1,17 +1,38 @@
-let btns = document.querySelectorAll(".btn");
+const btns = document.querySelectorAll(".btn");
+let keysDown = {};
 
 document.onkeydown = ((event) => {
-    btns.forEach((btn) => {
 
-        const key = btn.getAttribute('data-key');
+    const key = event.code;
 
-        if (event.code === key) {
+    if (!keysDown[key]) {
 
-            const sound = btn.getAttribute('data-sound');
-            const audio = new Audio(sound);
-            audio.play();
+        btns.forEach((btn) => {
 
-        }
+            const keyBtn = btn.getAttribute('data-key');
 
-    });
+            if (key === keyBtn) {
+
+                btn.animate({
+                    transform: ['scale(1)', 'scale(2)', 'scale(1)']
+                },
+                    {
+                        duration: 1000,
+                        easing: 'ease-in-out',
+                        iterations: 1
+                    });
+
+                const sound = btn.getAttribute('data-sound');
+                const audio = new Audio(sound);
+                audio.play();
+            }
+
+            keysDown[key] = true;
+        });
+    };
+});
+
+document.onkeyup = ((event) => {
+    const key = event.code;
+    keysDown[key] = false;
 });
